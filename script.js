f2f565ce28ca57300191a64cd041a7a6046de759
being2normal <hetder90@gmail.com>
2025-08-14 20:03:23 +0530

    add what was left

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// function add(){
-//     let n1 = parseInt(document.getElementById("1name").value)
-//     let n2 = parseInt(document.getElementById("2name").value)
-//     var sum = n1 + n2
-//     document.getElementById("ans").innerText=sum
-// }
+function add(){
+    let n1 = parseInt(document.getElementById("1name").value)
+    let n2 = parseInt(document.getElementById("2name").value)
+    var sum = n1 + n2
+    document.getElementById("ans").innerText=sum
+}
 
 function sub(){
     let n1 = parseInt(document.getElementById("1name").value)
     let n2 = parseInt(document.getElementById("2name").value)
     let min = n1 - n2
-    document.getElementById("ans").innerText = minrefc
+    document.getElementById("ans").innerText = min
+}
+function div(){
+    let n1 = parseInt(document.getElementById("1name").value)
+    let n2 = parseInt(document.getElementById("2name").value)
+    let di = n1 / n2
+    document.getElementById("ans").innerText = di
+}
+function sub(){
+    let n1 = parseInt(document.getElementById("1name").value)
+    let n2 = parseInt(document.getElementById("2name").value)
+    let mlt = n1 * n2
+    document.getElementById("ans").innerText = mlt
 }
