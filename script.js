5e537dbb990dfe3cd842dca9476b02cc7a59e61e
being2normal <hetder90@gmail.com>
2025-08-14 19:48:30 +0530

    add 2 sum cals

---
+++ script.js
@@ -0,0 +1,13 @@
+// function add(){
+//     let n1 = parseInt(document.getElementById("1name").value)
+//     let n2 = parseInt(document.getElementById("2name").value)
+//     var sum = n1 + n2
+//     document.getElementById("ans").innerText=sum
+// }
+
+function sub(){
+    let n1 = parseInt(document.getElementById("1name").value)
+    let n2 = parseInt(document.getElementById("2name").value)
+    let min = n1 - n2
+    document.getElementById("ans").innerText = minrefc
+}
